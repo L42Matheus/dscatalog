815e5f867bb5860462910ad1affc71d4e651b3bb
L42Matheus <77408554+L42Matheus@users.noreply.github.com>
2022-04-26 00:31:55 -0300

    request

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,13 +1,17 @@
 package com.devsuperior.dscatalog.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 public class Category implements Serializable {
 
     private Long id;
     private String name;
+
 
 }
